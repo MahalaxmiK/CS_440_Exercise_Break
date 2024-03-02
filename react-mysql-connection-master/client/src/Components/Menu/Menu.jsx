e600a6844cc83a3242e05a74c605a981c4b968fe
sakkk90 <99504056+sakkk90@users.noreply.github.com>
2024-03-01 22:25:41 -0600

    added exit button

--- react-mysql-connection-master/client/src/Components/Menu/Menu.jsx
+++ react-mysql-connection-master/client/src/Components/Menu/Menu.jsx
@@ -1,16 +1,21 @@
 import React, { useEffect, useState} from 'react'
 import './Menu.css'
 import Axios from "axios";
-
+import { ImCross } from 'react-icons/im'; 
 import meditationImg from "../Assets/music.jpeg"; 
 import musicImg from "../Assets/meditation.jpg"; 
 import googelImg from "../Assets/maps.avif"; 
 
 
 const Relax = () =>{
+    //  const [icon, setIcon] = useState(true);
 
     return(
         <div className = "container">
+            <button className='exit-icon'>
+                 <ImCross />
+                </button>
+
             <div className= "msg">Let's start your journey for the day!!</div>
             <div className="button-container">
                 <div className="image-container">
@@ -27,7 +32,7 @@ const Relax = () =>{
             
             <div className="button-container">
                 <div className="image-container">
-                <img src = {googelImg} alt="music"/>
+                <img src = {googelImg} alt="google"/>
                 </div>
                 <button className="g-btn"> Find Nearby Store</button>
             </div>
