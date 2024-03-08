d378540d4487203e5ceafa27c70c82655618ba67
sakkk90 <99504056+sakkk90@users.noreply.github.com>
2024-03-07 18:05:59 -0600

    linked to Relax page

--- react-mysql-connection-master/client/src/Components/Menu/Menu.jsx
+++ react-mysql-connection-master/client/src/Components/Menu/Menu.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState} from 'react'
 import './Menu.css'
 import Axios from "axios";
+import { useNavigate  } from "react-router-dom";
 import { ImCross } from 'react-icons/im'; 
 import meditationImg from "../Assets/music.jpeg"; 
 import musicImg from "../Assets/meditation.jpg"; 
@@ -8,33 +9,36 @@ import googelImg from "../Assets/maps.avif";
 
 
 const Relax = () =>{
-    //  const [icon, setIcon] = useState(true);
+    const navigate = useNavigate();
 
+    const handleRelaxButton =()=>{
+        navigate('/Relax')
+    }
     return(
-        <div className = "container">
+        <div className = "menu-container">
             <button className='exit-icon'>
                  <ImCross />
                 </button>
 
-            <div className= "msg">Let's start your journey for the day!!</div>
-            <div className="button-container">
+            <div className= "title-msg">Let's start your journey for the day!!</div>
+            <div className="m-button-container">
                 <div className="image-container">
-                <img src = {musicImg} alt="music"/>
+                <img className='m-img' src = {musicImg} alt="music"/>
                 </div>
-                <button className="w-btn">Start Workout</button>
+                <button className="m-btn">Start Workout</button>
             </div>
-            <div className = "button-container">
+            <div className = "m-button-container">
                 <div className = "image-container">
-                    <img src = {meditationImg} alt="meditation"/>
+                    <img className='m-img' src = {meditationImg} alt="meditation"/>
                 </div>
-                <button className="technique-btn">  Relaxation Techniques  </button>
+                <button className="m-btn" onClick={handleRelaxButton}>  Relaxation Techniques  </button>
             </div>
             
-            <div className="button-container">
+            <div className="m-button-container">
                 <div className="image-container">
-                <img src = {googelImg} alt="google"/>
+                <img className='m-img' src = {googelImg} alt="google"/>
                 </div>
-                <button className="g-btn"> Find Nearby Store</button>
+                <button className="m-btn"> Find Nearby Store</button>
             </div>
         </div>
     )
