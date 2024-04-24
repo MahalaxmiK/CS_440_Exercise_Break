01d9928859f2c5274b2a5ea6acf2100a6ce6ded7
Mahalaxmi Kalappareddigari <mkalap2@uic.edu>
2024-04-23 19:51:12 -0500

    unit testing attempted

--- react-mysql-connection-master/client/src/App.test.js
+++ react-mysql-connection-master/client/src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+it('renders without crashing', () => {
+  shallow(<App />);
+})
